52289be790fa0bfa77a7d10fccbcba4203e0b6d5
VeraGVG <verag@yandex.ru>
2025-08-02 23:06:36 +0300

    Add files via upload

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -1,25 +1,38 @@
-from typing import List
+"""blogicum URL Configuration
 
-from django.conf import settings
-from django.conf.urls.static import static
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+-----------------------------------------------------------------------------
+Добавлены пути для страниц регистрации, входа.
+Для регистрации используется кастомная view-функция
+"""
 from django.contrib import admin
-from django.urls import URLPattern, include, path
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from . import views
 
-handler403 = 'pages.views.permission_denied'
+app_name = 'blog'
 handler404 = 'pages.views.page_not_found'
 handler500 = 'pages.views.server_error'
-
-urlpatterns: List[URLPattern] = [
-    path('pages/', include('pages.urls', namespace='pages')),
-    path('', include('users.urls', namespace='users')),
-    path('', include('blog.urls', namespace='blog')),
-    path('auth/', include('django.contrib.auth.urls')),
+urlpatterns = [
+    path('', include('blog.urls')),
+    path('pages/', include('pages.urls')),
     path('admin/', admin.site.urls),
-]
-
-if settings.DEBUG:
-    import debug_toolbar
-
-    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('auth/', include('django.contrib.auth.urls')),
+    path(
+        'auth/registration/',
+        views.create_user,
+        name='registration',
+    ),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
